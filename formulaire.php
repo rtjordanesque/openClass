7f67c5aae9c697707d775b634ff0a45c8baebfab
rtjordanesque <rtjordanesque@gmail.com>
2018-10-26 12:15:15 +0200

    champ email supprimer pour formaire.php

--- formulaire.php
+++ formulaire.php
@@ -6,14 +6,12 @@
         <form name="inscription" method="post" action="saisie.php">
             Entrez votre pseudo : <input type="text" name="pseudo"/> <br/>
 	    Entrez votre ville : <input type="text" name="ville"/><br/>
-	    Entrez votre Email : <input type="text" name="email"/> <br/>
             <input type="submit" name="valider" value="OK"/>
         </form>
         <?php
         if(isset($_POST['valider'])){
             $pseudo=$_POST['pseudo'];
 	    $ville=$_POST['ville'];
-	    $email=$_POST['email'];
             echo 'Salut '. $pseudo.'de '. $ville.'<br/>Bienvenue sur mon site !';
         }
         ?>
